dbc3fc16cd2ea9943f4c0103e1430ef7fa90f2eb
Evan Rolfe <evanrolfe@gmail.com>
2017-04-12 15:35:30 +0100

    [webui] start kiwi_images/repositories controller

--- src/api/app/controllers/webui/kiwi_images/repositories_controller.rb
+++ src/api/app/controllers/webui/kiwi_images/repositories_controller.rb
@@ -3,19 +3,21 @@ module Webui
     class RepositoriesController < Webui::WebuiController
       before_action :set_project
       before_action :require_package
-      
+
       def new
       end
 
       def create
-        repository = Repository.find_by!(name: params[:repository_name])
-        kiwi_xml = @package.source_file('kiwi_config.xml', {})
-        kiwi_image = KiwiImage.from_xml(kiwi_xml)
-
-        kiwi_repo = KiwiImage::Repository.new(source: { path: "#{repository.project.name}/#{repository.name}" })
-        kiwi_image.repository << kiwi_repo
-
-        render json: kiwi_image
+        kiwi_image = Kiwi::Image.find_by(name: 'my_kiwi_file')
+        binding.pry
+        #repository = Repository.find_by!(name: params[:repository_name])
+        #kiwi_xml = @package.source_file('kiwi_config.xml', {})
+        #kiwi_image = KiwiImage.from_xml(kiwi_xml)
+#
+        #kiwi_repo = KiwiImage::Repository.new(source: { path: "#{repository.project.name}/#{repository.name}" })
+        #kiwi_image.repository << kiwi_repo
+#
+        #render json: kiwi_image
       end
     end
   end
