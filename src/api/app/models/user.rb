ae9c1ce4b9513efc22f92cec45d2a90b8ed3c00b
Eduardo Navarro <enavarro@suse.com>
2025-08-18 14:49:55 +0200

    Adjust devise defaults and run migration

--- src/api/app/models/user.rb
+++ src/api/app/models/user.rb
@@ -2,10 +2,8 @@ require 'kconv'
 require 'api_error'
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable
+
   include CanRenderModel
   include Flipper::Identifier
 
